d9d1260db1d1d000c9af362d7ef2d81907818c96
Julien <julien.quennehen@edu.devinci.fr>
2020-11-18 19:27:56 +0100

    update julien_part_4

---
+++ julien_part_4.php
@@ -0,0 +1,15 @@
+<div>
+    <p class="ourPartners">Our Partners</p>
+    <p class="Paragraph">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book.
+    </p>
+    <div class="partner">
+      <div class="foo blue"></div>
+      <div class="foo orange"></div>
+      <div class="foo green"></div>
+      <div class="foo yellow"></div>
+    </div>
+  </div>

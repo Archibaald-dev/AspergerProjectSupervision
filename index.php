51cf8a176e4562e091d348382999c0d49c8e31bf
Archibaald-dev <milo.lallement@yahoo.fr>
2020-11-18 15:33:15 +0100

    correction du code dégeu d'eliott

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+<?php include("header.php");
+include("milo_part_1.php");
+include("eliott_part_2.php");
+include("vincent_part_3.php");
+include("julien_part_4.php");
+include("florent_part_5.php");
+?>
+
+</body>
+</html>

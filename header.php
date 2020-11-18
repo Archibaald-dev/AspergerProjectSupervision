75fc876407cd01d7c8329135b6d8b97a4a034e7f
Archibaald-dev <milo.lallement@yahoo.fr>
2020-11-18 16:16:16 +0100

    header

--- header.php
+++ header.php
@@ -1,22 +1,21 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Syndrome <span class="sr-only">(current)</span></a>
+        <a class="nav-link green" href="#">Syndrome <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Game</a>
+        <a class="nav-link orange" href="#">Game</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-      <li class="nav-item dl">
-        <a class="nav-link" href="#">DOWNLOAD THE GAME</a>
+        <a class="nav-link blue" href="#">Contact</a>
       </li>
     </ul>
+    
+        <a class="DL" href="#">DOWNLOAD THE GAME</a>
   </div>
 </nav> 
---
+++ milo_part_1.php
@@ -0,0 +1,5 @@
+<div class="header">
+    <div class="extend"></div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim iste animi eligendi aspernatur natus est et obcaecati dignissimos deserunt, facilis maxime voluptate praesentium. Assumenda eaque corrupti quasi eum autem porro!</p>
+    <button>Discover the game</button>
+</div>

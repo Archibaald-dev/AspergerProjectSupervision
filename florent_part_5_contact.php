a87abbc2e3eb0101a835be54d59f365e5fffdafb
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-11-24 17:09:17 +0000

    Prepared Form

---
+++ florent_part_5_contact.php
@@ -0,0 +1,30 @@
+<p class="form_title h1 text-center">CONTACT US</p>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 mx-auto">
+            <div class="form_contact col-8 mx-auto">
+                <div class="row">
+
+                    <div class="test col-6">
+
+                    </div>
+                    <div class="test2 col-6">
+
+                    </div>
+                    <div class="test3 col-12">
+
+                    </div>
+                    <div class="test4 col-12">
+
+                    </div>
+
+                    <div class="test5 col-12">
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

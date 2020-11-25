371def05bd0539a885b073605eea9126019b6818
eliott11 <55191511+eliott11@users.noreply.github.com>
2020-11-25 16:00:34 +0100

    update

--- eliott_part_2.php
+++ eliott_part_2.php
@@ -1,5 +1,21 @@
 <div>
+    <h1 class="text-center"><font face="GoodDog New">A new way to learn about the syndrome</font></h1>
     <div>
-        <h1 class="text-center"><font face="GoodDog New">A new way to learn about the syndrome</font></h1>
+        <div>
+            <p class=""><font face="GoodDog New">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</font></p>
+        </div>
+        <div>
+            <img class="" src="">
+        </div>
+    </div>
+    <div>
+        <div>
+            <img class="" src="">
+        </div>
+        <div>
+            <p class=""><font face="GoodDog New">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</font></p>
+        </div>
     </div>
 </div>
